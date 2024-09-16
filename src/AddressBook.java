62c02cf73cade38ff984af08d55e70aa53c76a4f
Kaif Ali <kaifali@cmail.carleton.ca>
2024-09-16 13:00:55 -0400

    commiting change

---
+++ src/AddressBook.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+
+public class AddressBook {
+
+    private ArrayList<BuddyInfo> buddys;
+
+    public AddressBook() {
+        this.buddys = new ArrayList<>();
+    }
+
+    public void addBuddy(BuddyInfo buddy) {
+        if (buddy != null) {
+            this.buddys.add(buddy);
+        }
+    }
+
+    public BuddyInfo removeBuddy(int index) {
+        if (index >= 0 && index < this.buddys.size()) {
+            return this.buddys.remove(index);
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        AddressBook addressBook = new AddressBook();
+        BuddyInfo buddy1 = new BuddyInfo("Homer","19 Homerson St", 513311043);
+        BuddyInfo buddy2 = new BuddyInfo("Hammy","24 Homerson St", 513311078);
+
+        addressBook.addBuddy(buddy1);
+        addressBook.addBuddy(buddy2);
+
+
+        System.out.println("Address Book");
+        System.out.println("Buddies in the address book:");
+        for (BuddyInfo buddy : addressBook.buddys) {
+            System.out.println(buddy.getName());
+        }
+    }
+}
